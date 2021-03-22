84d0b0312def05e9864cfc8ab3bc7e0dbd4a4665
Peter Spada <peterspa@microsoft.com>
2021-03-22 15:46:11 -0700

    Hot keys for fixed window sizes

--- retron/source/main.cpp
+++ retron/source/main.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "source/core/app_service.h"
 #include "source/core/options.h"
 #include "source/main.h"
 #include "source/states/app_state.h"
@@ -9,11 +10,11 @@ static const std::string_view NOESIS_NAME = "d704047b-5bd2-4757-9858-6a7d86cdd00
 static const std::string_view NOESIS_KEY = "B/+52Cy2udj84658Qvc+tLTo9Yv7ZNqBhmykexFIA6nbnVl0";
 static std::weak_ptr<retron::app_state> weak_app_state;
 
-namespace
+namespace assets
 {
-    namespace assets_main
+    namespace controls
     {
-#include "assets.res.h"
+#include "controls.res.h"
 
         static std::shared_ptr<::ff::data_base> data()
         {
@@ -21,9 +22,10 @@ namespace
         }
     }
 
-    namespace assets_graphics
+
+    namespace game_spec
     {
-#include "assets.graphics.res.h"
+#include "game_spec.res.h"
 
         static std::shared_ptr<::ff::data_base> data()
         {
@@ -31,9 +33,9 @@ namespace
         }
     }
 
-    namespace assets_xaml
+    namespace graphics
     {
-#include "assets.xaml.res.h"
+#include "graphics.res.h"
 
         static std::shared_ptr<::ff::data_base> data()
         {
@@ -41,9 +43,9 @@ namespace
         }
     }
 
-    namespace assets_values
+    namespace xaml
     {
-#include "values.res.h"
+#include "xaml.res.h"
 
         static std::shared_ptr<::ff::data_base> data()
         {
@@ -54,10 +56,10 @@ namespace
 
 static void register_components()
 {
-    ff::resource_objects::register_global_dict(assets_main::data());
-    ff::resource_objects::register_global_dict(assets_graphics::data());
-    ff::resource_objects::register_global_dict(assets_xaml::data());
-    ff::resource_objects::register_global_dict(assets_values::data());
+    ff::resource_objects::register_global_dict(assets::controls::data());
+    ff::resource_objects::register_global_dict(assets::game_spec::data());
+    ff::resource_objects::register_global_dict(assets::graphics::data());
+    ff::resource_objects::register_global_dict(assets::xaml::data());
 
     Noesis::RegisterComponent<Noesis::EnumConverter<retron::game_flags>>();
     Noesis::RegisterComponent<Noesis::EnumConverter<retron::game_players>>();
@@ -122,9 +124,96 @@ ff::init_ui_params retron::get_ui_params()
 
 #if !UWP_APP
 
+static void set_window_client_size(HWND hwnd, const ff::point_int& new_size)
+{
+    RECT rect{ 0, 0, new_size.x, new_size.y }, rect2;
+    const DWORD style = static_cast<DWORD>(GetWindowLong(hwnd, GWL_STYLE));
+    const DWORD exStyle = static_cast<DWORD>(GetWindowLong(hwnd, GWL_EXSTYLE));
+
+    HMONITOR monitor = ::MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+    MONITORINFO mi{};
+    mi.cbSize = sizeof(mi);
+
+    if (monitor &&
+        ::AdjustWindowRectExForDpi(&rect, style, FALSE, exStyle, ::GetDpiForWindow(hwnd)) &&
+        ::GetMonitorInfo(monitor, &mi) &&
+        ::GetWindowRect(hwnd, &rect2))
+    {
+        ff::rect_int monitor_rect(mi.rcWork.left, mi.rcWork.top, mi.rcWork.right, mi.rcWork.bottom);
+        ff::rect_int window_rect(rect2.left, rect2.top, rect2.left + rect.right - rect.left, rect2.top + rect.bottom - rect.top);
+
+        window_rect = window_rect.move_inside(monitor_rect).crop(monitor_rect);
+
+        ::SetWindowPos(hwnd, nullptr, window_rect.left, window_rect.top, window_rect.width(), window_rect.height(),
+            SWP_ASYNCWINDOWPOS | SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOOWNERZORDER);
+    }
+}
+
+static void handle_window_message(ff::window_message& msg)
+{
+    switch (msg.msg)
+    {
+        case WM_SYSKEYDOWN:
+            if ((msg.wp == VK_DELETE || msg.wp == VK_PRIOR || msg.wp == VK_NEXT) &&
+                retron::app_service::get().game_spec().allow_debug &&
+                ::GetKeyState(VK_SHIFT) < 0 &&
+                !ff::app_render_target().full_screen())
+            {
+                // Shift-Alt-Del: Force 1080p client area
+                // Shift-Alt-PgUp|PgDown: Force multiples of 1080p client area
+
+                RECT rect;
+                if (::GetClientRect(msg.hwnd, &rect))
+                {
+                    const std::array<ff::point_int, 4> sizes
+                    {
+                        ff::point_int(480, 270),
+                        ff::point_int(960, 540),
+                        ff::point_int(1920, 1080),
+                        ff::point_int(3840, 2160),
+                    };
+
+                    const ff::point_int old_size(rect.right, rect.bottom);
+                    ff::point_int new_size = (msg.wp == VK_DELETE) ? sizes[2] : old_size;
+
+                    if (msg.wp == VK_PRIOR)
+                    {
+                        for (auto i = sizes.crbegin(); i != sizes.crend(); i++)
+                        {
+                            if (i->x < new_size.x)
+                            {
+                                new_size = *i;
+                                break;
+                            }
+                        }
+                    }
+                    else if (msg.wp == VK_NEXT)
+                    {
+                        for (auto i = sizes.cbegin(); i != sizes.cend(); i++)
+                        {
+                            if (i->x > new_size.x)
+                            {
+                                new_size = *i;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (new_size != old_size)
+                    {
+                        ::set_window_client_size(msg.hwnd, new_size);
+                    }
+                }
+            }
+            break;
+    }
+}
+
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, LPWSTR, int)
 {
     ff::init_app init_app(retron::get_app_params(), retron::get_ui_params());
+    ff::signal_connection message_connection = ff::window::main()->message_sink().connect(::handle_window_message);
+
     return ff::handle_messages_until_quit();
 }
 
